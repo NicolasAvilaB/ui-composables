95ea2273f443447d158364276255417cf7bd7e8a
NicolasAvilaB <nicolasab1996@gmail.com>
2024-04-06 03:57:59 -0300

    fix color

--- app/src/main/java/com/items/ui/composables/ui/theme/Theme.kt
+++ app/src/main/java/com/items/ui/composables/ui/theme/Theme.kt
@@ -5,13 +5,10 @@ import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -25,7 +22,8 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    inverseSurface = Purple40,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -51,14 +49,16 @@ fun UicomposablesTheme(
                 darkColorScheme(
                     primary = Purple80,
                     secondary = PurpleGrey80,
-                    tertiary = Pink80
+                    tertiary = Pink80,
+                    inverseSurface = Purple80,
                 )
             } else {
                 lightColorScheme(
                     primary = Purple40,
                     secondary = PurpleGrey40,
-                    tertiary = Pink40
-                )
+                    tertiary = Pink40,
+                    inverseSurface = Purple40,
+                    )
             }
         }
 
