098bcdcf9d1fe45567bee29e734064b627356036
NicolasAvilaB <nicolasab1996@gmail.com>
2024-06-09 20:17:44 -0400

    fix gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     apply( from = "dependencies.gradle.kts")
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+    }
 }
 
 plugins {
